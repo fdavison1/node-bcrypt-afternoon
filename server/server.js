a3c100b01f97a438d106917489217244cb76ce2e
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-11-06 13:23:02 -0700

    database setup

--- server/server.js
+++ server/server.js
@@ -2,12 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const session = require('express-session')
 const massive = require('massive')
-const {SERVER_PORT} = process.env
+const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
 
 const app = express()
 
 //middleware
 app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: true, 
+    saveUninitialized: false
+}))
+
+//massive
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    app.listen(SERVER_PORT, ()=> console.log(`listening on port ${SERVER_PORT}`))
+})
 
 
-app.listen(SERVER_PORT, ()=> console.log(`listening on port ${SERVER_PORT}`))
